b6787e24c81c5964c971abe99befcc8186141e45
anwholesquare <anwholesquare@gmail.com>
2020-04-26 13:59:13 +0600

    Updates

---
+++ precache-manifest.56ab0e3a821fc45ad6c66636ba31cbe8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293a7699c8ac92279637b1fd4bec948a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3298b647a2b03b23eead",
+    "url": "/static/css/main.194d964f.chunk.css"
+  },
+  {
+    "revision": "930138164425a40d5750",
+    "url": "/static/js/2.2acf0d67.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.2acf0d67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3298b647a2b03b23eead",
+    "url": "/static/js/main.e800076d.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "e5802db6eb04e655ff03c4b589ff74ce",
+    "url": "/static/media/image.e5802db6.png"
+  }
+]);
